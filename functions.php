80bd4904b87ec199faff0ae8e18ffb35e84f72fa
Nicos <nikolicsinisa@outlook.com>
2019-01-21 22:20:30 +0100

    treca promena

--- functions.php
+++ functions.php
@@ -94,7 +94,7 @@ function yogaflex_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'yogaflex_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'yogaflex_content_width', 767 );
 }
 add_action( 'after_setup_theme', 'yogaflex_content_width', 0 );
 
