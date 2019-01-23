17d5225636bb2f9354d88df0364b45ae487a38ba
Nicos <nikolicsinisa@outlook.com>
2019-01-23 22:37:42 +0100

    Header transformacija 1. deo

--- functions.php
+++ functions.php
@@ -123,22 +123,35 @@ function yogaflex_scripts() {
 
 	// enqueue styles
 
-	wp_enqueue_style( 'yogaflex-style', get_stylesheet_uri(). 'style.css' );
-	wp_enqueue_style( 'yogaflex-style-linearicons', get_stylesheet_uri(). '/style/linearicons.css' );
-	wp_enqueue_style( 'yogaflex-style-font-awesome', get_stylesheet_uri(). '/style/font-awesome.min.css' );
-	wp_enqueue_style( 'yogaflex-style-bootstrap', get_stylesheet_uri(). '/style/bootstrap.css' );
-	wp_enqueue_style( 'yogaflex-style-magnific-popup', get_stylesheet_uri(). '/style/magnific-popup.css' );
-	wp_enqueue_style( 'yogaflex-style-nice-select', get_stylesheet_uri(). '/style/nice-select.css' );
-	wp_enqueue_style( 'yogaflex-style-animate', get_stylesheet_uri(). '/style/animate.min.css' );
-	wp_enqueue_style( 'yogaflex-style-carousel', get_stylesheet_uri(). '/style/owl.carousel.css' );
-	wp_enqueue_style( 'yogaflex-style-jquery', get_stylesheet_uri(). '/style/jquery-ui.css' );
-	wp_enqueue_style( 'yogaflex-style-main', get_stylesheet_uri(). '/style/main.css' );
+	wp_enqueue_style( 'yogaflex-style', get_template_directory_uri(). 'style.css' );
+	wp_enqueue_style( 'yogaflex-style-linearicons', get_template_directory_uri(). '/style/linearicons.css' );
+	wp_enqueue_style( 'yogaflex-style-font-awesome', get_template_directory_uri(). '/style/font-awesome.min.css' );
+	wp_enqueue_style( 'yogaflex-style-bootstrap', get_template_directory_uri(). '/style/bootstrap.css' );
+	wp_enqueue_style( 'yogaflex-style-magnific-popup', get_template_directory_uri(). '/style/magnific-popup.css' );
+	wp_enqueue_style( 'yogaflex-style-nice-select', get_template_directory_uri(). '/style/nice-select.css' );
+	wp_enqueue_style( 'yogaflex-style-animate', get_template_directory_uri(). '/style/animate.min.css' );
+	wp_enqueue_style( 'yogaflex-style-carousel', get_template_directory_uri(). '/style/owl.carousel.css' );
+	wp_enqueue_style( 'yogaflex-style-jquery', get_template_directory_uri(). '/style/jquery-ui.css' );
+	wp_enqueue_style( 'yogaflex-style-main', get_template_directory_uri(). '/style/main.css' );
 
 	// enqueue scripts
 
 	wp_enqueue_script( 'yogaflex-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'yogaflex-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'yogaflex-jquery-224', get_template_directory_uri() . '/js/vendor/jquery-2.2.4.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-cloudflare', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array(), '0.1', false );
+	wp_enqueue_script( 'yogaflex-bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA', array(), '0.1', false );
+	wp_enqueue_script( 'yogaflex-easing', get_template_directory_uri() . '/js/easing.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-hoverintent', get_template_directory_uri() . '/js/hoverIntent.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-superfish', get_template_directory_uri() . '/js/superfish.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-ajaxchimp', get_template_directory_uri() . '/js/jquery.ajaxchimp.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-magnific-popup', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-tabs', get_template_directory_uri() . '/js/jquery.tabs.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-nice-select', get_template_directory_uri() . '/js/jquery.nice-select.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-mail', get_template_directory_uri() . '/js/mail-script.js', array(), '0.1', true );
+	wp_enqueue_script( 'yogaflex-main', get_template_directory_uri() . '/js/main.js', array(), '0.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -22,9 +22,31 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'yogaflex' ); ?></a>
+	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'yogaflex' ); ?></a> -->
 
 	<header id="masthead" class="site-header">
+
+	<div class="header-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-sm-6 col-8 header-top-left no-padding">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-lg-6 col-sm-6 col-4 header-top-right no-padding">
+						<a href="tel:+880 1234 654 953">
+							<span class="text">+880 1234 654 953</span>
+						</a>
+						<a class="book-now" href="#">Book Now</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
@@ -45,7 +67,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'yogaflex' ); ?></button>
+			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'yogaflex' ); ?></button> -->
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
