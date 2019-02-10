188ea604a41c7a8d2e638876204f2f074b845cce
Nicos <nikolicsinisa@outlook.com>
2019-02-10 22:15:40 +0100

    male izmene u searchform i style

--- searchform.php
+++ searchform.php
@@ -9,10 +9,9 @@
 
 ?>
 
-<?php $unique_id = esc_attr( wp_unique_id( 'search-form-' ) ); ?>
+<?php $unique_id = esc_attr( wp_unique_id( 'search-form' ) ); ?>
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
-	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Posts', 'placeholder', 'yogaflex' ); ?>" value="<?php echo get_search_query(); ?>" name="search" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Posts'" >
+	<input type="text" placeholder="<?php echo esc_attr_x( 'Search Posts', 'placeholder', 'yogaflex' ); ?>" value="<?php echo get_search_query(); ?>" name="search" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Posts'" >
 	<button type="submit"><i class="fa fa-search"></i></button>
 </form>
