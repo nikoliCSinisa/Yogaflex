127daa3176e114701a19a242549f74a28c0bb669
Nicos <nikolicsinisa@outlook.com>
2019-07-24 22:11:06 +0200

    pravljenje custom meta boxes

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -14,6 +14,10 @@
         
         add_filter( 'manage_yogaflex-contact_posts_columns', 'yogaflex_set_contact_columns' );
         add_action( 'manage_yogaflex-contact_posts_custom_column', 'yogaflex_contact_custom_column', 10, 2 );
+
+        add_action( 'add_meta_boxes', 'yogaflex_contact_add_meta_box' );
+
+        add_action( 'save_post', 'yogaflex_save_contact_email_data' );
     }
 
 
@@ -54,20 +58,56 @@ function yogaflex_contact_custom_column( $column, $post_id){
 
     switch( $column){
 
-        case 'title' :
-            echo 'Name';
-            break;
-
-        case 'subject' :
-            echo 'Subject';
-            break;
-
         case 'message' :
             echo get_the_excerpt();
             break;
 
         case 'email' :
-            echo 'email adress';
+            $email = get_post_meta( $post_id, '_contact_email_value_key', true );
+            echo '<a href="mailto:' . $email . '">' . $email . '</a>';
             break;
     }
+}
+
+/* Contact Meta boxes */
+
+function yogaflex_contact_add_meta_box(){
+    add_meta_box( 'contact_email', 'User Email', 'yogaflex_contact_email_callback', 'yogaflex-contact', 'side', 'default' );
+}
+
+function yogaflex_contact_email_callback( $post ){
+    wp_nonce_field( 'yogaflex_save_contact_email_data', 'yogaflex_contact_email_meta_box_nonce' );
+
+    $value = get_post_meta( $post->ID, '_contact_email_value_key', true );
+
+
+    echo '<label for="yogaflex_contact_email_field">User email address: </label>';
+    echo '<input type="email" id="yogaflex_contact_email_field" name="yogaflex_contact_email_field" value="' . esc_attr( $value ). '" size="25" />';
+}
+
+function yogaflex_save_contact_email_data( $post_id ){
+
+    if( ! isset( $_POST['yogaflex_contact_email_meta_box_nonce'] ) ){
+        return;
+    }
+
+    if( ! wp_verify_nonce( $_POST['yogaflex_contact_email_meta_box_nonce'], 'yogaflex_save_contact_email_data' ) ){
+        return;
+    }
+
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+        return;
+    }
+
+    if( ! current_user_can( 'edit_post', $post_id ) ){
+        return;
+    }
+
+    if( ! isset( $_POST['yogaflex_contact_email_field'] ) ){
+        return;
+    }
+
+    $my_data = sanitize_text_field(  $_POST['yogaflex_contact_email_field'] );
+
+    update_post_meta( $post_id, '_contact_email_value_key', $my_data );
 }
